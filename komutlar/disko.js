2d5cf6c64bdf6dcb2a4fa2daa0ca96eef1735816
BoqQinq <44748025+BoqQinq@users.noreply.github.com>
2018-11-04 18:20:22 +0200

    Delete disko.js

--- komutlar/disko.js
+++
@@ -1,9 +0,0 @@
-    client.on('message', async msg => {
-    if (msg.content.toLowerCase() === prefix + "disko") {
-   if (msg.channel.type === "dm") return;
-  const rol = 'Disqo' // Rol ismi buraya
-  setInterval(() => {
-      msg.guild.roles.find(s => s.name === rol).setColor("RANDOM")
-      }, 9000);
-  }
-});
