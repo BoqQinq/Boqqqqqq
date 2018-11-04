f89c3e0c341dd6f74643719bb4838fa49d402128
BoqQinq <44748025+BoqQinq@users.noreply.github.com>
2018-11-04 16:21:33 +0200

    Delete sigara.js

--- komutlar/sigara.js
+++
@@ -1,9 +0,0 @@
-client.on('message', msg => {
-if (msg.content.toLowerCase() === prefix + "sigara") {
-msg.channel.send('🚬 ☁:cloud:☁')
-.then(nmsg => nmsg.edit('🚬 ☁:cloud:☁'))
-.then(nmsg => nmsg.edit('🚬 ☁:cloud:'))
-.then(nmsg => nmsg.edit('🚬 ☁'))
-.then(nmsg => nmsg.edit('**Sigaram bitti** | **Sigara İçmeyiniz.** 🚭 **Sigara Sağlığa Zararlıdır**'));
-}
-});
